1ee87cfdd1a926a37fbc4e76887a21502a1b67cf
sunyajun <sunyajun@bilibili.com>
2020-06-03 21:39:47 +0800

    feat: add bio server and client;

--- netty/src/main/java/bio/IOClient.java
+++ netty/src/main/java/bio/IOClient.java
@@ -11,14 +11,16 @@ import java.util.Date;
 public class IOClient {
 
     public static void main(String[] args) {
+        System.out.println("====> server start ... ");
+
         new Thread(() -> {
             try {
                 Socket socket = new Socket("127.0.0.1", 8000);
                 while (true) {
                     try {
-                        socket.getOutputStream().write((new Date() + ": hello world").getBytes());
                         Thread.sleep(2000);
-
+                        socket.getOutputStream().write((new Date() + ": hello world").getBytes());
+                        System.out.println("====> client write data... ");
                     } catch (Exception e) {
                     }
                 }
--- netty/src/main/java/bio/IOServer.java
+++ netty/src/main/java/bio/IOServer.java
@@ -1,5 +1,8 @@
 package bio;
 
+
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -10,7 +13,9 @@ import java.net.Socket;
  * @date 2020/4/20 1:46 PM
  */
 public class IOServer {
+
     public static void main(String[] args) throws Exception {
+        System.out.println("====> server start ... ");
 
         ServerSocket serverSocket = new ServerSocket(8000);
 
@@ -18,18 +23,21 @@ public class IOServer {
         new Thread(() -> {
             while (true) {
                 try {
+                    System.out.println("=====> server listen client...");
                     // (1) 阻塞方法获取新的连接
                     Socket socket = serverSocket.accept();
 
                     // (2) 每一个新的连接都创建一个线程，负责读取数据
                     new Thread(() -> {
+                        System.out.println("====> 监听到client... ");
                         try {
                             int len;
                             byte[] data = new byte[1024];
                             InputStream inputStream = socket.getInputStream();
                             // (3) 按字节流方式读取数据
                             while ((len = inputStream.read(data)) != -1) {
-                                System.out.println(new String(data, 0, len));
+                                System.out.println("=====> read data:" + new String(data, 0, len));
+
                             }
                         } catch (IOException e) {
                         }
